a317806f16f7d97a580a3b5117e75cf1f4dc4d6a
ZjanPreijde <j.preijde2@upcmail.nl>
2018-02-08 21:06:27 +0100

    Added seed data, added superagent to packages

---
+++ db/seed.js
@@ -0,0 +1,8 @@
+// db/seed.js
+const request = require('superagent')
+const user    = require('./fixtures/user.json')
+const recipes = require('./fixtures/recipes.json')
+
+const createUrl = (path) => {
+  return `${process.env.HOST || `http://localhost:${process.env.PORT || 3030}`}${path}`
+}
