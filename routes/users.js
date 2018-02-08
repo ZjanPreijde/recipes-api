ddcaf39708a95457e356f002f249dbd649d32ece
ZjanPreijde <j.preijde2@upcmail.nl>
2018-02-08 16:16:07 +0100

    Added get users/ and get users/:id to routes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,30 @@
 const router = require('express').Router()
 const { User } = require('../models')
 
-router.post('/users', (req, res, next) => {
+router
+  // Index of users
+  .get('/users', (req, res, next) => {
+    User.find()
+      // Newest users first
+      .sort({ createdAt: -1 })
+      // Send the data in JSON format
+      .then((users) => res.json(users))
+      // Throw a 500 error if something goes wrong
+      .catch((error) => next(error))
+  })
+  // Show 1 user
+  .get('/users/:id', (req, res, next) => {
+    const id = req.params.id
+
+    User.findById(id)
+      .then( (user) => {
+        if (!user) { return next() }
+        res.json({name: user.name, email: user.email, createdAt: user.createdAt, updatedAt: user.updatedAt})
+
+      })
+      .catch((error) => next(error))
+  })
+  .post('/users', (req, res, next) => {
     User.register(new User({name: req.body.name, email: req.body.email}), req.body.password, (err, user) => {
       if (err) {
         err.status = 422
